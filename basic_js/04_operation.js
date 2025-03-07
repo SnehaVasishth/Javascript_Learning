790f8b086f662def5f86910f580e3dc666860629
SnehaVasishth <snehavasishth03@gmail.com>
2025-03-07 14:16:49 +0530

    operation and conversion and some confusing conversions in js

---
+++ basic_js/04_operation.js
@@ -0,0 +1,43 @@
+let value=3
+let negValue=-value
+
+console.log(negValue)//-3
+
+console.log(2+2)//4
+console.log(2-2)//0
+console.log(2/2)//1
+console.log(2*2)//4
+console.log(2**2)//4
+console.log(2%2)//0
+
+let str1="sneha"
+let str2= "vasishth"
+let str3= str1+str2
+console.log(str3)//snehavasishth
+console.log(typeof(str3))//string
+
+console.log("1"+2)//12
+console.log(typeof("1"+2))//string
+
+console.log(2+"1") //21
+console.log(typeof(2+"1"))//string
+
+console.log("1"+2+2)//122
+console.log(typeof("1"+2+2))//string
+
+console.log(2+2+"1")//41
+console.log(typeof(2+2+"1"))//string
+
+console.log(true)//true
+console.log(+true)//1
+console.log(typeof(+true))//number
+
+console.log(+false)//0
+
+console.log(+"")//0
+console.log(typeof(+""))//number
+
+let num1,num2,num3
+
+num1=num2=num3=2+2
+console.table([num1,num2,num3])// 4 4 4
